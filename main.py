30f6c812064c205470ca5227f80538486112c45d
voneAbides <44479267+voneAbides@users.noreply.github.com>
2020-11-29 22:54:46 +0100

    Implementation of classification of two categories 1 or -1

---
+++ main.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_derive(x):
+    return x * (1 - x)
+
+
+training_inputs = np.array([[-1, -1, -1],
+                            [-1, -1, 1],
+                            [-1, 1, -1],
+                            [-1, 1, 1],
+                            [1, -1, -1],
+                            [1, -1, 1],
+                            [1, 1, -1],
+                            [1, 1, 1]])
+
+training_outputs = np.array([[-1, -1, -1, 1, -1, 1, 1, 1]]).T
+
+np.random.seed(24)
+
+synaptic_weights = 2 * np.random.random((3, 1)) - 1
+
+print('Starting weights:')
+print(synaptic_weights)
+
+
+for iteration in range(1000000):
+    input_l = training_inputs
+
+    outputs = sigmoid(np.dot(input_l, synaptic_weights))
+
+    error = training_outputs - outputs
+
+    adjustments = error * sigmoid_derive(outputs)
+
+    synaptic_weights += np.dot(input_l.T, adjustments)
+
+
+print('Synaptics weights after:')
+print(synaptic_weights)
+
+print('Output after:')
+print(outputs)
